912ab69a806aaaaa5bb6d77666b3f9c6f2e82d62
Girish-123-dev <gnalawad@asu.edu>
2025-07-31 13:16:13 -0700

    Completed the integration of GeminiApi with the sevice and able to get a valid response back

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Smart Email Generator
+To get the Gemini API key and URL. 
+
+Go to Official Gemini page and create a API key and copy the uri from the main page. Add the uri and the  API key in the application.properties to run the application. 
+
--- src/main/java/com/email/app/EmailGeneratorController.java
+++ src/main/java/com/email/app/EmailGeneratorController.java
@@ -1,14 +1,29 @@
-package com.email.app;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/email")
-public class EmailGeneratorController {
-    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
-        return ResponseEntity.ok("");
-    }
-}
+//package com.email.app;
+//
+//import jakarta.annotation.PostConstruct;
+//import lombok.AllArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/email")
+//@AllArgsConstructor
+//public class EmailGeneratorController {
+//
+//
+//    private final EmailGeneratorService emailGeneratorService;
+//
+//    @PostMapping("/generate")
+//    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
+//        String response = emailGeneratorService.generateEmailReply(emailRequest);
+//        return ResponseEntity.ok(response   );
+//    }
+//
+//    @GetMapping("/test")
+//    public String testEndpoint() {
+//        return "Controller is active!";
+//    }
+//
+//
+//}
